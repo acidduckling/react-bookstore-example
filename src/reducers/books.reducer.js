c0b679c573648564962a64488f23ae5a2fa3d755
Clayton Wakefield <acidduckling@gmail.com>
2018-07-29 12:55:32 +1000

    implement reducers

---
+++ src/reducers/books.reducer.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript the GOod Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 39 },
+    { title: 'Dark Tower', pages: 85 },
+    { title: 'Eloquent Ruby', pages: 1 }
+  ];
+}
